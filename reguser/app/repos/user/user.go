ae5f09c597eb82b2512412fee4aeddc557d1647f
saintvrai <ishanovairat@gmail.com>
2022-11-12 18:14:19 +0300

    DC444 | repositories and settings1

--- reguser/app/repos/user/user.go
+++ reguser/app/repos/user/user.go
@@ -1,1 +1,27 @@
 package user
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID   uuid.UUID
+	Name string
+	Data string
+}
+type UserStore interface {
+	Create(u User) (*uuid.UUID, error)
+}
+type Users struct {
+	ustore UserStore
+}
+
+func (us *Users) Create(u User) (*User, error) {
+	id, err := us.ustore.Create(u)
+	if err != nil {
+		return nil, fmt.Errorf("create user error: %w", err)
+	}
+	u.ID = *id
+	return &u, nil
+}
